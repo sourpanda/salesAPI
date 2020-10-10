bbf4913de9bbc4ac74716aae1be65c9e85489fd6
sourpanda <47013843+sourpanda@users.noreply.github.com>
2020-10-09 21:48:30 -0400

    release candidate

--- public/js/main.js
+++ public/js/main.js
@@ -14,7 +14,7 @@ var perPage = 10;
 
 var saleTemplateString = `
 <% _.each(sales, function(sale){ %>
-    <tr data-id=\"<%= sale._id %>\">
+    <tr data-id="<%= sale._id %>">
     <td><%= sale.customer.email %></td>
     <td><%= sale.storeLocation %></td>
     <td><%= sale.items.length %></td>
@@ -22,12 +22,12 @@ var saleTemplateString = `
     </td></tr>
 <% }); %>`;
 
-var modelString = `<h4>Customer</h4>
+var modalString = `<h4>Customer</h4>
 <strong>email:</strong> <%= sale.customer.email %><br>
 <strong>age:</strong> <%= sale.customer.age %><br>
 <strong>satisfaction:</strong> <%= sale.customer.satisfaction %> / 5
 <br><br>
-<h4> Items: <%= sale.items.length %> </h4>
+<h4> Items: $<%= sale.total %> </h4>
 <table class="table">
 <thead>
 <tr>
@@ -39,22 +39,24 @@ var modelString = `<h4>Customer</h4>
 <tbody>
 <% _.each(sale.items, function(item){ %>
     <tr>
-    <td><%= sale.item.name %></td>
-    <td><%= sale.item.price %></td>
-    <td><%= sale.item.quantity %></td>
+    <td><%= item.name %></td>
+    <td><%= item.price %></td>
+    <td><%= item.quantity %></td>
     </tr>
 <% }); %>
 </tbody>
 </table>`;
 
-var saleModelBodyTemplate = _.template(modelString);
+var saleModalBodyTemplate = _.template(modalString);
 
 var saleTableTemplate = _.template(saleTemplateString);
+var salesArray = [];
 
 function loadSaleData() {
   fetch(`/api/sales/?page=${page}&perPage=${perPage}`)
     .then((response) => response.json())
     .then((sales) => {
+      salesArray = sales;
       var tableData = saleTableTemplate({ sales: sales });
       $("tbody").html(tableData);
       $("#current-pg").html(page);
@@ -66,4 +68,34 @@ function loadSaleData() {
 
 $(document).ready(function () {
   loadSaleData();
+  $("table > tbody").on("click", "tr", function () {
+    var clickedID = $(this).attr("data-id");
+    var clickedSale = salesArray.find((sale) => sale._id == clickedID);
+    clickedSale.total = 0;
+    for (var i = 0; i < clickedSale.items.length; i++) {
+      clickedSale.total +=
+        clickedSale.items[i].price * clickedSale.items[i].quantity;
+    }
+    clickedSale.total = clickedSale.total.toFixed(2);
+    // modal stuff here
+    $("h4.modal-title").html(`Sale: ${clickedSale._id}`);
+    var modalData = saleModalBodyTemplate({ sale: clickedSale });
+    $("div.modal-body").html(modalData);
+    $("div#sale-modal").modal({
+      backdrop: false,
+      keyboard: false,
+    });
+  });
+
+  // paginate
+  $("a#previous-pg").on("click", function () {
+    page > 1 ? page-- : page;
+    console.log(page);
+    loadSaleData();
+  });
+  $("a#next-pg").on("click", function () {
+    page++;
+    console.log(page);
+    loadSaleData();
+  });
 });
